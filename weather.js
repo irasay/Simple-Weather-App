23d27e18e4bd4f913a475bdfa3975eb182804b53
Ivanna Rasay <rasay.ivanna@hotmail.com>
2017-02-13 21:00:54 -0500

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,150 @@
+/*main*/
+var temperature;
+var location;
+var humidity;
+var icon;
+var wind;
+var direction;
+
+/*not main*/
+var high;
+var low;
+var con;
+
+/*API*/
+var APIKEY = "b833afa850a9acf8e46e4f71b031ed47";
+
+function displayTime() {
+
+}
+
+function updateByCity(city) {
+        var link = "http://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&APPID=" 
+        + APIKEY ;
+
+        var link = "http://api.openweathermap.org/data/2.5/forecast?q="
+        + city
+        + "&APPID="
+        + APIKEY ;
+
+        sendRequest(link);
+        sendForecast(link2);
+}
+
+function sendRequest(link) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                        var data = JSON.parse(xmlhttp.responseText) ;
+                        var weather = {} ;
+                        weather.icon = data.weather[0].id;
+                        weather.humidity = data.main.humidity;
+                        weather.wind = Math.round(3.6*(data.wind.speed));
+                        weather.direction = changeDir(data.wind.deg);
+                        weather.loc = data.name;
+                        weather.temperature = celsius(data.main.temp); 
+                        update(weather);
+                }
+        };
+        xmlhttp.open("GET", link,  true);
+        xmlhttp.send(); 
+}
+
+function sendForecast(link2) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                        var data = JSON.parse(xmlhttp.responseText);
+                        var weather = {} ;
+                        var date = Array.from(data.main.dt_txt);
+                        for 
+
+
+                }
+        }
+}
+
+function updateForecast(weather) {
+        high.innerHTML = weather.high;
+        low.innerHTML = weather.low;
+        con.src = "WeatherIcons/" + weather.con + ".png";
+}
+
+function update(weather) {
+        wind.innerHTML = weather.wind;
+        direction.innerHTML = weather.direction;
+        humidity.innerHTML = weather.humidity;
+        loc.innerHTML = weather.loc;
+        temperature.innerHTML = weather.temperature;
+        icon.src = "WeatherIcons/" + weather.icon + ".png";
+}
+
+window.onload = function () {
+        icon = document.getElementById('icon');
+        temperature = document.getElementById('temperature');
+        loc = document.getElementById('location');
+        humidity = document.getElementById('humidity');
+        wind = document.getElementById('wind');
+        direction = document.getElementById('direction');
+
+        con = document.getElementById('con');
+        high = document.getElementById('high');
+        low = document.getElementById('low');
+
+        updateByCity("Brampton");
+}
+
+function celsius(tmp) {
+        return Math.round(tmp - 273.15);
+}
+
+function farenheit(tmp) {
+        return Math.round(tmp*(9/5) - 459.67);
+}
+
+function changeDir(deg) {
+        var range = 360/8;
+        var low = 360 - range/2;
+        var high = (low + range)%360;
+
+        var cardinality = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
+        for (i in cardinality) {
+                if (deg >= low && deg < high) 
+                        return cardinality[i];
+
+                low = (low + range) %360;
+                high = (high + range)%360;
+        }
+        return "N";
+}
+
+/*temp until I figure it out*/
+function updateByCityF(city) {
+        var link = "http://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&APPID=" 
+        + APIKEY ;
+
+        sendRequestF(link);
+}
+
+function sendRequestF(link) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                        var data = JSON.parse(xmlhttp.responseText) ;
+                        var weather = {} ;
+                        weather.icon = data.weather[0].id;
+                        weather.humidity = data.main.humidity;
+                        weather.wind = Math.round(3.6*(data.wind.speed));
+                        weather.direction = changeDir(data.wind.deg);
+                        weather.loc = data.name;
+                        weather.temperature = farenheit(data.main.temp); 
+                        update(weather);
+                }
+        };
+        xmlhttp.open("GET", link,  true);
+        xmlhttp.send(); 
+} /*end of temp*/
